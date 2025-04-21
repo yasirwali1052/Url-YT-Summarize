4f25f8065c9dc909f629c79901232165c07c8f35
yasirwali1052 <yasirwali301302@gmail.com>
2025-04-22 00:12:37 +0500

    First commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import validators
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain_groq import ChatGroq
+from langchain.chains.summarize import load_summarize_chain
+from langchain_community.document_loaders import YoutubeLoader, WebBaseLoader  # Use WebBaseLoader instead of UnstructuredURLLoader
+
+## streamlit APP
+st.set_page_config(page_title="LangChain: Summarize Text From YT or Website", page_icon="🦜")
+st.title("🦜 LangChain: Summarize Text From YT or Website")
+st.subheader('Summarize URL')
+
+## Get the Groq API Key and url(YT or website)to be summarized
+with st.sidebar:
+    groq_api_key = st.text_input("Groq API Key", value="", type="password")
+
+generic_url = st.text_input("URL", label_visibility="collapsed")
+
+## Gemma Model Using Groq API
+llm = ChatGroq(model="llama-3.3-70b-versatile", groq_api_key=groq_api_key)
+
+prompt_template = """
+Provide a summary of the following content in 300 words:
+Content:{text}
+"""
+prompt = PromptTemplate(template=prompt_template, input_variables=["text"])
+
+if st.button("Summarize the Content from YT or Website"):
+    ## Validate all the inputs
+    if not groq_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the information to get started")
+    elif not validators.url(generic_url):
+        st.error("Please enter a valid URL. It can be a YT video URL or website URL")
+    else:
+        try:
+            with st.spinner("Waiting..."):
+                ## loading the website or YT video data
+                if "youtube.com" in generic_url:
+                    loader = YoutubeLoader.from_youtube_url(generic_url, add_video_info=True)
+                else:
+                    loader = WebBaseLoader(generic_url)  # Using WebBaseLoader instead of UnstructuredURLLoader
+                docs = loader.load()
+
+                ## Chain For Summarization
+                chain = load_summarize_chain(llm, chain_type="stuff", prompt=prompt)
+                output_summary = chain.run(docs)
+
+                st.success(output_summary)
+        except Exception as e:
+            st.exception(f"Exception:{e}")
